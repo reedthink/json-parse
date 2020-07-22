bee9119166f61f95fb8c30ed36dffdc26ca1ff62
DevOnLinux <reedthink@qq.com>
2020-07-22 13:20:02 +0800

    完成第五课'

--- learn01/leptjson.c
+++ learn01/leptjson.c
@@ -33,7 +33,7 @@
         c->top = head;    \
         return ret;       \
     } while (0)
-
+/*这个结构保存的是读入的字符流*/
 typedef struct
 {
     /* const char*是指向常量的指针,而不是指针本身为常量,可以不被初始化.该指针可以指向常量也可以指向变量,只是从该指针的角度而言,它所指向的是常量。
@@ -256,6 +256,56 @@ static int lept_parse_string(lept_context *c, lept_value *v)
         }
     }
 }
+static int lept_parse_value(lept_context *c, lept_value *v);
+
+static int lept_parse_array(lept_context *c, lept_value *v)
+{
+    size_t size = 0, i;
+    int ret;
+    EXPECT(c, '[');
+    lept_parse_whitespace(c);
+    if (*c->json == ']')
+    {
+        c->json++;
+        v->type = LEPT_ARRAY;
+        v->u.a.size = 0;
+        v->u.a.e = NULL;
+        return LEPT_PARSE_OK;
+    }
+    for (;;)
+    {
+        lept_value e;
+        lept_init(&e);
+        if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK)
+            break;
+        memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
+        size++;
+        lept_parse_whitespace(c);
+        if (*c->json == ',')
+        {
+            c->json++;
+            lept_parse_whitespace(c);
+        }
+        else if (*c->json == ']')
+        {
+            c->json++;
+            v->type = LEPT_ARRAY;
+            v->u.a.size = size; /*Todo 那么union是如何判断使用哪一段*/
+            size *= sizeof(lept_value);
+            memcpy(v->u.a.e = (lept_value *)malloc(size), lept_context_pop(c, size), size);
+            return LEPT_PARSE_OK;
+        }
+        else
+        {
+            ret = LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+            break;
+        }
+    }
+    for (i = 0; i < size; i++)
+        lept_free((lept_value *)lept_context_pop(c, sizeof(lept_value)));
+    return ret;
+}
+
 /*功能：解析*/
 static int lept_parse_value(lept_context *c, lept_value *v)
 {
@@ -271,6 +321,8 @@ static int lept_parse_value(lept_context *c, lept_value *v)
         return LEPT_PARSE_EXPECT_VALUE;
     case '\"':
         return lept_parse_string(c, v);
+    case '[':
+        return lept_parse_array(c, v);
     default:
         return lept_parse_number(c, v);
     }
@@ -285,11 +337,36 @@ void lept_set_string(lept_value *v, const char *s, size_t len)
     v->u.s.len = len;
     v->type = LEPT_STRING;
 }
+
+size_t lept_get_array_size(const lept_value *v)
+{
+    assert(v != NULL && v->type == LEPT_ARRAY);
+    return v->u.a.size;
+}
+lept_value *lept_get_array_element(const lept_value *v, size_t index)
+{
+    assert(v != NULL && v->type == LEPT_ARRAY);
+    assert(index < v->u.a.size);
+    return &v->u.a.e[index];
+}
+
 void lept_free(lept_value *v)
 {
+    size_t i;
     assert(v != NULL);
-    if (v->type == LEPT_STRING)
+    switch (v->type)
+    {
+    case LEPT_STRING:
         free(v->u.s.s);
+        break;
+    case LEPT_ARRAY:
+        for (i = 0; i < v->u.a.size; i++)
+            lept_free(&v->u.a.e[i]);
+        free(v->u.a.e);
+        break;
+    default:
+        break;
+    }
     v->type = LEPT_NULL;
 }
 
--- learn01/test.c
+++ learn01/test.c
@@ -31,6 +31,7 @@ static int test_pass = 0;
     EXPECT_EQ_BASE(sizeof(expect) - 1 == alength && memcmp(expect, actual, alength) == 0, expect, actual, "%s")
 #define EXPECT_TRUE(actual) EXPECT_EQ_BASE((actual) != 0, "true", "false", "%s")
 #define EXPECT_FALSE(actual) EXPECT_EQ_BASE((actual) == 0, "false", "true", "%s")
+#define EXPECT_EQ_SIZE_T(expect, actual) EXPECT_EQ_BASE((expect) == (actual), (size_t)expect, (size_t)actual, "%zu")
 
 #define TEST_ERROR(error, json)                      \
     do                                               \
@@ -115,6 +116,12 @@ static void test_parse_invalid_value()
     TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "inf");
     TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "NAN");
     TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "nan");
+
+    /* invalid value in array */
+#if 1
+    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "[1,]");
+    TEST_ERROR(LEPT_PARSE_INVALID_VALUE, "[\"a\", nul]");
+#endif
 }
 
 static void test_parse_root_not_singular()
@@ -236,6 +243,59 @@ static void test_parse_invalid_unicode_surrogate()
     TEST_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\\uDBFF\"");
     TEST_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE, "\"\\uD800\\uE000\"");
 }
+static void test_parse_miss_comma_or_square_bracket()
+{
+#if 1
+    TEST_ERROR(LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,"[1");
+    TEST_ERROR(LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,"[1}");
+    TEST_ERROR(LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,"[1 2");
+    TEST_ERROR(LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET,"[[]");
+
+#endif
+}
+
+static void test_parse_array()
+{
+    size_t i, j;
+    lept_value v;
+
+    lept_init(&v);
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "[ ]"));
+    EXPECT_EQ_INT(LEPT_ARRAY, lept_get_type(&v));
+    EXPECT_EQ_SIZE_T(0, lept_get_array_size(&v));
+    lept_free(&v);
+
+    lept_init(&v);
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "[ null , false , true , 123 , \"abc\" ]"));
+    EXPECT_EQ_INT(5, lept_get_array_size(&v));
+    EXPECT_EQ_SIZE_T(5, lept_get_array_size(&v));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(lept_get_array_element(&v, 0)));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(lept_get_array_element(&v, 1)));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(lept_get_array_element(&v, 2)));
+    EXPECT_EQ_INT(LEPT_NUMBER, lept_get_type(lept_get_array_element(&v, 3)));
+    EXPECT_EQ_INT(LEPT_STRING, lept_get_type(lept_get_array_element(&v, 4)));
+    EXPECT_EQ_DOUBLE(123.0, lept_get_number(lept_get_array_element(&v, 3)));
+    EXPECT_EQ_STRING("abc", lept_get_string(lept_get_array_element(&v, 4)), lept_get_string_length(lept_get_array_element(&v, 4)));
+    lept_free(&v);
+
+    lept_init(&v);
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "[ [ ] , [ 0 ] , [ 0 , 1 ] , [ 0 , 1 , 2 ] ]"));
+    EXPECT_EQ_INT(LEPT_ARRAY, lept_get_type(&v));
+    EXPECT_EQ_SIZE_T(4, lept_get_array_size(&v));
+    for (i = 0; i < 4; i++)
+    {
+        lept_value *a = lept_get_array_element(&v, i);
+        EXPECT_EQ_INT(LEPT_ARRAY, lept_get_type(a));
+        EXPECT_EQ_SIZE_T(i, lept_get_array_size(a));
+        for (j = 0; j < i; j++)
+        {
+            lept_value *e = lept_get_array_element(a, j);
+            EXPECT_EQ_INT(LEPT_NUMBER, lept_get_type(e));
+            EXPECT_EQ_DOUBLE((double)j, lept_get_number(e));
+        }
+    }
+    lept_free(&v);
+}
 
 static void test_access_null()
 {
@@ -287,6 +347,7 @@ static void test_parse()
     test_parse_false();
     test_parse_number();
     test_parse_string();
+    test_parse_array();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
@@ -296,6 +357,7 @@ static void test_parse()
     test_parse_invalid_string_char();
     test_parse_invalid_unicode_hex();
     test_parse_invalid_unicode_surrogate();
+    test_parse_miss_comma_or_square_bracket();
 }
 static void test_access()
 {
